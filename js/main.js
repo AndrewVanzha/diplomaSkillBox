722e236b507691cd9d3be13e6978944e96539e95
AndrewW <andy10243@mail.ru>
2019-10-18 11:51:34 +0300

    сделал обработку клика кнопки Узнать больше, поправил другие кнопки

--- js/main.js
+++ js/main.js
@@ -195,132 +195,6 @@ function setLeftPaddingForPictures() { // управление левым бло
 
 }
 
-// расчет траектории смещения для символов, class__signString:String - класс "красного"/"белого" символа
-// sign_startCoords:Array - начальная позиция, sign_endCoords:Array - конечная позиция
-// startWindowWidth - ширина окна для начальной позиции, endWindowWidth - ширина окна для конечной позиции
-function calculateCoordsSigns(startWindowWidth, sign_startCoords, endWindowWidth, sign_endCoords) {
-  var i = 0;
-  var paramK;
-  var paramC;
-
-  for (i = 0; i < sign_startCoords.length; i++) {
-    paramK = (sign_startCoords[i][0] - sign_endCoords[i][0]) / (startWindowWidth - endWindowWidth);
-    paramC = sign_startCoords[i][0] - paramK * startWindowWidth;
-    var coordX = paramK * windowWidth + paramC;
-
-    paramK = (sign_startCoords[i][1] - sign_endCoords[i][1]) / (startWindowWidth - endWindowWidth);
-    paramC = sign_startCoords[i][1] - paramK * startWindowWidth;
-    var coordY = paramK * windowWidth + paramC;
-
-    paramK = (sign_startCoords[i][2] - sign_endCoords[i][2]) / (startWindowWidth - endWindowWidth);
-    paramC = sign_startCoords[i][2] - paramK * startWindowWidth;
-    var degS = paramK * windowWidth + paramC;
-
-    paramK = (sign_startCoords[i][3] - sign_endCoords[i][3]) / (startWindowWidth - endWindowWidth);
-    paramC = sign_startCoords[i][3] - paramK * startWindowWidth;
-    var opacityE = paramK * windowWidth + paramC;
-
-    signsParameters[i][0] = coordX;
-    signsParameters[i][1] = coordY;
-    signsParameters[i][2] = degS;
-    signsParameters[i][3] = opacityE;
-
-  }
-  //console.log(startWindowWidth);
-  //console.log(endWindowWidth);
-}
-
-// формирование траектории смещения для символов, class__signString:String - класс "красного"/"белого" символа - не используется
-// sign_startCoords:Array - начальная позиция, sign_endCoords:Array - конечная позиция
-// startWindowWidth - ширина окна для начальной позиции, endWindowWidth - ширина окна для конечной позиции
-function moveSign(class__signString, startWindowWidth, sign_startCoords, endWindowWidth, sign_endCoords) {
-  var i = 0;
-  var classString = '';
-  //var classString = '.small-elements ';
-
-  classString += class__signString; // формирую строку нужного класса
-  calculateCoordsSigns(startWindowWidth, sign_startCoords, endWindowWidth, sign_endCoords); // расчет конечных параметров позиции тега
-  for (i = 0; i < signsParameters.length; i++) {
-    signsParameters[i][0] = signsParameters[i][0] + 110;
-    signsParameters[i][1] = signsParameters[i][1] + 280;
-  }
-
-  for (i = 0; i < sign_startCoords.length; i++) {
-    var act__classString = classString + (i + 1); // формирую строку нужного класса
-
-    $(act__classString).css({
-      'left': signsParameters[i][0],
-      'top': signsParameters[i][1],
-      'opacity': signsParameters[i][3]
-    });
-
-  }
-  //console.log(startWindowWidth);
-  //console.log(endWindowWidth);
-}
-
-function scatterSigns(classString) {
-  var i = 0;
-  var act__classString = '';
-  //console.log(windowWidth);
-
-  for (i = 0; i < signsParameters.length; i++) {
-    act__classString = classString + (i + 1); // формирую строку нужного класса
-
-    $(act__classString).css({
-      'transform': 'matrix(1, ' + signsParameters[i][2] + ', ' + -signsParameters[i][2] + ', 1, ' + signsParameters[i][0] + ',' + signsParameters[i][1] + ')',
-      'opacity': signsParameters[i][3],
-      'transition': 'transform 2s ease-out'
-    });
-  }
-
-}
-
-// формирование позиции для символов, class__signString:String - класс "красного"/"белого" символа 
-// sign_startCoords:Array - начальная позиция (широкий экран), sign_endCoords:Array - конечная позиция (узкий экран)
-// startWindowWidth - ширина окна для широкого экрана, endWindowWidth - ширина окна для узкого экрана
-function deploySigns(class__signString, startWindowWidth, sign_startCoords, endWindowWidth, sign_endCoords) {
-  var i = 0;
-  var classString = '.central-container__redtag.small-sign';
-
-  classString += class__signString; // формирую строку нужного класса
-  calculateCoordsSigns(startWindowWidth, sign_startCoords, endWindowWidth, sign_endCoords); // расчет конечных параметров позиции тега
-  scatterSigns(class__signString);
-
-  //console.log(class__signString);
-  //console.log(startWindowWidth);
-  //console.log(endWindowWidth);
-}
-
-function setCoordsForSigns() { // управление символами в персональном и ценовом разделах 
-  var i = 0;
-
-  if (windowWidth > 1920) {
-    // двигаю все 5 красных символов в персональном разделе
-    deploySigns('.central-container__redtag.small-sign', desktopWidth, redSigns_wideWindow, tabletWidth, redSigns_middleWindow);
-    // двигаю все 5 белых символов в ценовом разделе
-    deploySigns('.central-container__whitetag.small-sign', desktopWidth, whiteSigns_wideWindow, tabletWidth, whiteSigns_middleWindow);
-    //moveSign('.red-sign', desktopWidth, redSigns_wideWindow, tabletWidth, redSigns_middleWindow); 
-    //moveSign('.white-sign', desktopWidth, whiteSigns_wideWindow, tabletWidth, whiteSigns_middleWindow); 
-
-  } else if (windowWidth <= 1920 && windowWidth > 1170) {
-    deploySigns('.central-container__redtag.small-sign', desktopWidth, redSigns_wideWindow, tabletWidth, redSigns_middleWindow);
-    deploySigns('.central-container__whitetag.small-sign', desktopWidth, whiteSigns_wideWindow, tabletWidth, whiteSigns_middleWindow);
-
-  } else if (windowWidth <= 1170 && windowWidth > 1005) { // 1024px
-    deploySigns('.central-container__redtag.small-sign', desktopWidth, redSigns_wideWindow, tabletWidth, redSigns_middleWindow);
-    deploySigns('.central-container__whitetag.small-sign', desktopWidth, whiteSigns_wideWindow, tabletWidth, whiteSigns_middleWindow);
-
-  } else {
-    deploySigns('.central-container__redtag.small-sign', desktopWidth, redSigns_wideWindow, telWidth, redSigns_narrowWindow);
-    deploySigns('.central-container__whitetag.small-sign', desktopWidth, whiteSigns_wideWindow, telWidth, whiteSigns_narrowWindow);
-    //moveSign('.red-sign', desktopWidth, redSigns_wideWindow, telWidth, redSigns_narrowWindow); // двигаю все 5 красных символов в персональном разделе
-    //moveSign('.white-sign', desktopWidth, whiteSigns_wideWindow, telWidth, whiteSigns_narrowWindow); // двигаю все 5 белых символов в ценовом разделе
-  }
-
-  //console.log(windowWidth);
-}
-
 function manageSwiperPaginationAndButtons() { // управление окном pagination и кнопками в слайдере
   if (windowWidth > 1250) {
     if (!($('div.swiper-pagination').is('.hide-element'))) {
@@ -366,7 +240,6 @@ $(document).ready(function () {
   setLeftPaddingForPictures();
   moveFXinCentralContainer();
   changeTelClasses();
-  //setCoordsForSigns();
   manageSwiperPaginationAndButtons();
   //setLeftEdgePopupMenu();
   //manageHeaderPhoneLine();
@@ -390,7 +263,6 @@ $(window).resize(function () {
   setLeftPaddingForPictures();
   moveFXinCentralContainer();
   changeTelClasses();
-  //setCoordsForSigns();
   manageSwiperPaginationAndButtons();
   //setLeftEdgePopupMenu();
   //manageHeaderPhoneLine();
@@ -422,7 +294,26 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function () {
+$(document).ready(function () { // обработка клика по кнопке central-container__personal_button
+  $('.central-container__personal_button').click(function (ev) {
+    //console.log(ev.target);
+    $('#modal__knowmore').slideDown(300);
+    $('.bg_popup').fadeIn(300);
+  });
+
+  $('.modal__knowmore_close').click(function () {
+    $('#modal__knowmore').slideUp(300);
+  });
+
+  $('.bg_popup').click(function (ev) {
+    //console.log(ev.target);
+    $('#modal__knowmore').slideUp(300);
+    $('.bg_popup').fadeOut(300);
+  });
+
+});
+
+$(document).ready(function () { // обработка клика по кнопке deal-container__boxes_button
   $('.deal-container__boxes_button').click(function (ev) {
     //console.log(ev.target);
     $('#modal__price').slideDown(300);
@@ -441,8 +332,8 @@ $(document).ready(function () {
 
 });
 
-$(document).ready(function () {
-  $('.project-button-position').click(function (ev) {
+$(document).ready(function () { // обработка клика по кнопке examples-container__projectbutton
+  $('.examples-container__projectbutton').click(function (ev) {
     //console.log(ev.target);
     $('#modal__project').slideDown(300);
     $('.bg_popup').fadeIn(300);
